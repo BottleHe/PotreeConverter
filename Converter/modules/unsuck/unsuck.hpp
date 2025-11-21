de12fbef3a8f2de3d48d4060d826fd7171060978
BottleHe <bottle@fridayws.com>
2025-11-21 19:36:01 +0800

    perf: Mac 兼容支持。

--- Converter/modules/unsuck/unsuck.hpp
+++ Converter/modules/unsuck/unsuck.hpp
@@ -16,6 +16,9 @@
 #include <cstdint>
 #include <cstring>
 
+#include "cpu.h"
+#include "memory.h"
+
 using std::cout;
 using std::endl;
 using std::to_string;
@@ -43,6 +46,8 @@ static double Infinity = std::numeric_limits<double>::infinity();
 constexpr auto fseek_64_all_platforms = fseeko64;
 #elif defined(WIN32)
 constexpr auto fseek_64_all_platforms = _fseeki64;
+#elif defined(__APPLE__)
+constexpr auto fseek_64_all_platforms = fseeko;
 #endif
 
 
